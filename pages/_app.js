a89698a62c067a51552c2ae30ce5cda7fd25b1e5
Rene Balog-Dutombe <balogdrene@gmail.com>
2022-06-09 11:32:33 +0200

    hover transition and head content added

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,20 @@
-// import "../styles/reset.css"
+import Head from "next/head"
 import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>QR code component</title>
+        <meta
+          name="description"
+          content="A perfect first challenge if you're new to HTML and CSS. The card layout doesn't shift, so it's ideal if you haven't learned about building responsive layouts yet."
+        />
+        <link rel="icon" href="favicon-32x32.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
